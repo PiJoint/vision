b1e1aff243bf7de8c7f2d940bc6f06cb2e2736e5
Alessandro Mizzaro <developer@alessandromizzaro.me>
2023-02-08 16:04:41 +0100

    Fix point cloud, return response

--- include/pijoint_vision/vision/point_cloud.py
+++ include/pijoint_vision/vision/point_cloud.py
@@ -6,21 +6,14 @@ from geometry_msgs.msg import Point
 
 import numpy as np
 
-def todtype7(msg):
-    dtype = np.dtype(np.float32)
-    dtype = dtype.newbyteorder('>' if msg.is_bigendian else '<')
-    img = np.ndarray(shape=(msg.row_step * msg.height, 1),
-                           dtype=dtype, buffer=msg.data)
-    if msg.is_bigendian == (sys.byteorder == 'little'):
-        img = img.byteswap().newbyteorder()
-    return img
 
-
-def read_float(msg, start, len=12):
+def read_floats(msg, start, len=12):
     a = msg.data
 
     dtype = np.dtype(np.float32)
     dtype = dtype.newbyteorder('>' if msg.is_bigendian else '<')
+    if msg.is_bigendian == (sys.byteorder == 'little'):
+            im = im.byteswap().newbyteorder()
     return np.ndarray(shape=(1,3),
                            dtype=dtype, buffer=msg.data[start: start+len])[0]
 
@@ -29,20 +22,11 @@ def read_float(msg, start, len=12):
 
 
 def pixel2cloud(point_cloud: PointCloud2, x: int, y: int):
-    print(point_cloud.point_step)
-    
-
-    w = point_cloud.width
-    h = point_cloud.height
     
     array_position = y * point_cloud.row_step + x * point_cloud.point_step
 
 
-    data = read_float(point_cloud, array_position)
-    print(data)
-    #x = array_position + point_cloud.fields[0].offset
-    #y = array_position + point_cloud.fields[1].offset
-    #z = array_position + point_cloud.fields[2].offset
+    data = read_floats(point_cloud, array_position)
 
     return \
         data[0], data[1], data[2]
--- src/service.py
+++ src/service.py
@@ -14,12 +14,17 @@ from pijoint_vision.vision import pixel2cloud
 
 bridge = CvBridge()
 
+classifier = Model('src/best.pt', 0.30)
+up_classifier = Model('src/up_and_down.pt', 0.50)
 
 
-def pose(up_classifier, img0, depth, box):
-    (x,y,x1,y1)  = box
+def pose(img0, depth, box):
+
+    depth, point_cloud = depth
+    x,y,x1,y1  = box
+
+
     cropped = img0[int(y):int(y1), int(x):int(x1)]
-
     ob = up_classifier.detect_object(cropped)
     # TODO get bigger box
 
@@ -27,24 +32,19 @@ def pose(up_classifier, img0, depth, box):
         c, _ = o
 
         # TODO opencv2
-        yaw = 0 
-        pitch = 0
-        roll = 0
-
-        x = 0
-        y = 0 
-        z = 0
+        #   - GET CENTER
+        #   - GET YAW
+        cx,cy = x+ ( (x1-x) // 2), y+ ((y1-y)//2)
+        px,py,pz = pixel2cloud(point_cloud, cx,cy)
 
         return Pose(
-            Point(x,y,z),
-            Rotation(yaw, pitch, roll)
+            Point(px,py,pz),
+            Rotation(0, 0, 0)
         )
 
 
-def object_detection(classifiers, req):
+def object_detection(req):
     rospy.loginfo("Starting detection...")
-
-    classifier, up_c = classifiers
     rgbd = rospy.wait_for_message('/pjoint/_internal/camera', RGBD)
     
     try:
@@ -52,12 +52,12 @@ def object_detection(classifiers, req):
         left = bridge.imgmsg_to_cv2(rgbd.image, 'bgr8')
         depth = bridge.imgmsg_to_cv2(rgbd.depth)
         point_cloud = rgbd.point_cloud
+
         cv2.resize(depth,(1920,1080))
         cv2.resize(left,(1920,1080))
-        # point_cloud = rgbd.point_cloud
 
     except CvBridgeError as e:
-        return ObjectDetectionResponse([])
+        return ObjectDetectionResponse(False, [])
     else:
         # DETECT AND PUBLISH
         objects = []
@@ -66,42 +66,24 @@ def object_detection(classifiers, req):
         for o in ob:
             
             c, (x,y,x1,y1) = o
-            print('BOX', x,y,x1,y1)
-            cx,cy = x+ ( (x1-x) // 2), y+ ((y1-y)//2)
-
-
-            
-            px,py,pz = pixel2cloud(point_cloud, cx,cy)
-            print('POINTCLOUD: ', px, py, pz, 'CENTER', cx,cy, 'DEPTH', depth[cy,cx])
-            print(sqrt(px**2 + pz**2 + py**2))
-
             rospy.loginfo("Found %d" % c)
 
-            # rotation = pose(up_c, left, depth, (x,y,x1,y1))
-
+            rotation = pose(left, (depth, point_cloud), (x,y,x1,y1))
           
-            #objects.append(
-            #    Object(c, rotation)
-            #)
+            objects.append(
+                Object(c, rotation)
+            )
     return ObjectDetectionResponse(True, objects)
             
 
 def init():
     rospy.loginfo("Loading models...")
 
-    classifier = Model('src/best.pt', 0.30)
-    up_classifier = Model('src/up_and_down.pt', 0.50)
+  
     rospy.loginfo("Fused...")
 
     rospy.loginfo("Loading services...")
-    rospy.Service('object_detection', ObjectDetection, partial(object_detection, (classifier, up_classifier)))
+    rospy.Service('object_detection', ObjectDetection,object_detection)
 
-
-
-if __name__ == '__main__':
-    rospy.wait_for_service('object_detection')
-    service = rospy.ServiceProxy('object_detection', ObjectDetection)
-
-    response =  service(True)
 
 
